c7121102d1a30c2b8c60bd9d7d00a72a608941da
ruzina75@gmail.com <ruzina75@gmail.com>
2020-03-24 14:46:28 +0000

    Programming Fundamentals One Complete

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,37 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+function makeHalfPrice(price) {
+    const discount = 0.5 * price;
+    return price + discount;
+}
+makeHalfPrice(100);
+
+function countBooks(bookTitles) {
+    return bookTitles.length;
+}
+
+function isInStock(book) {
+if (book.quantity) {
+    return true;
+}
+return false;
+}
+
+function getTotalOrderPrice(price, quantity) {
+    //rounded to two decimal places as money
+    return Math.round(price * quantity * 1.2 * 100) / 100;
+}
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,48 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+
+  test("returns 240 when passed 200", () => {
+    expect(utils.addVAT(200)).toBe(240);
+  });
 });
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+})
+
+describe("utils.makeHalfPrice", () => {
+  test("returns 150 when passed 100", () => {
+    expect(utils.makeHalfPrice(100)).toBe(150);
+  });
+})
+
+describe("utils.countBooks", () => {
+  test("returns book titles and number of books in the array", () => {
+    expect(utils.countBooks(["Harry Potter and the Philosophers Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban"])).toBe(3);
+  });
+})
+
+describe("utils.isInStock", () => {
+  test("returns if book is in stock", () => {
+    const book = {
+      title: "The Stone Diaries",
+      author: "Carol Shields",
+      yearOfPublication: 1993,
+      quantity: 0
+    };    
+    expect(utils.isInStock(book)).toBe(false);
+  });
+})
+
+describe("utils.getTotalOrderPrice", () => {
+  test("returns total price with VAT applied", () => {
+    expect(utils.getTotalOrderPrice(18, 2)).toBe(43.20);
+  });
+})
